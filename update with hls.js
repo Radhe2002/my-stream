4c5e412107d094b7a2dfbb51433f1a07871b553b
Radhe2002 <108767712+Radhe2002@users.noreply.github.com>
2025-04-13 17:26:16 +0530

    Update update with hls.js

--- update with hls.js
+++ update with hls.js
@@ -1,30 +1,13 @@
 <!DOCTYPE html>
 <html>
 <head>
-  <title>Live Stream</title>
-  <script src="https://cdn.jsdelivr.net/npm/hls.js@latest"></script>
+    <title>Live Stream</title>
 </head>
 <body>
-  <h1>My Live Stream</h1>
-  <video id="video" width="640" height="360" controls autoplay></video>
-
-  <script>
-    var video = document.getElementById('video');
-    var videoSrc = 'https://dai.fancode.com/linear/hls/pb/event/dvF4wcTrSVCbUUBU-QiSxA/stream/73a7978f-4e46-4c51-b068-31b11a2a236a:SIN2/master.m3u8?hdnea=st=1744525417~exp=1744525717~acl=*~id=027103a9-24a7-42d7-a1e7-7977898b6329~hmac=d249cdfd53bdbe40b57e111fabb64fa08fcedcfdaab6c009096def37c0f4512a';
-
-    if (Hls.isSupported()) {
-      var hls = new Hls();
-      hls.loadSource(videoSrc);
-      hls.attachMedia(video);
-      hls.on(Hls.Events.MANIFEST_PARSED,function() {
-        video.play();
-      });
-    } else if (video.canPlayType('application/vnd.apple.mpegurl')) {
-      video.src = videoSrc;
-      video.addEventListener('loadedmetadata',function() {
-        video.play();
-      });
-    }
-  </script>
+    <h1>My Live Stream</h1>
+    <video width="640" height="360" controls autoplay>
+        <source src="https://dai.fancode.com/linear/hls/pb/event/bXwJzw5HTzKk4pDsYgSDjw/stream/ed242981-6cea-4849-97b8-2cd9fc4d9052:TPE/master.m3u8?hdnea=st=1744541190~exp=1744541490~acl=*~id=69de2e1b-5413-4060-b1b4-359141b255a6~hmac=fe02e892d57a112a5bdafa98add36b0e75f1c276486a8f9c4ebadb13d479ca4c" type="application/x-mpegURL">
+        Your browser does not support the video tag.
+    </video>
 </body>
 </html>
